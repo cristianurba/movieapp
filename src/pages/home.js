ab8c9bb5273885c6581657c5f8a1a21963c2fd0c
cristianurba <cristianurba@gmail.com>
2021-07-20 13:16:56 +0200

    Página detalle de cada película con modal del trailer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,30 @@
 import React from "react";
+import {Row, Col} from "antd";
 import useFetch from "../hooks/useFecth";
 import {URL_API, API} from "../utils/constants";
 
 import SliderMovies from "../components/SliderMovies";
+import MovieList from "../components/MovieList";
+import Footer from "../components/footer";
 
 export default function Home(){
 
     const newMovies = useFetch(`${URL_API}/movie/now_playing?api_key=${API}&language=es-ES&page=1`);
+    const popularMovies = useFetch(`${URL_API}/movie/popular?api_key=${API}&language=es-ES&page=1`);
+    const topRatedMovies =  useFetch(`${URL_API}/movie/top_rated?api_key=${API}&language=es-ES&page=1`);
     
-
     return (
         <>
             <SliderMovies movies={newMovies} />
+            <Row>
+                <Col span={12}>
+                    <MovieList title="Películas Populares" movies={popularMovies}/>
+                </Col>
+                <Col span={12}>
+                    <MovieList title="Películas más votadas" movies={topRatedMovies}/>
+                </Col>
+            </Row>
+            <Footer />
         </>
     );
 }
